0614c949c10e0c061b9f19fb76395ff0dd785360
jhonrexk <57768368+jhonrexk@users.noreply.github.com>
2019-11-23 06:57:15 +0800

    Update sleak.sh

--- sleak.sh
+++ sleak.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 echo "Removing Old Theme"
-rm -rf /home/panel/html/asset
+rm -rf /home/panel/html/assets
 rm -rf /home/panel/html/view
 rm -rf /home/panel/html/tmp/*
-echo "Installing Sleak Themes"
+echo "Installing Template"
 cd ~
-mkdir Sleak
-cd Sleak
+mkdir baa
+cd baa
 rm -rf *
-wget https://github.com/jhonrexk/ocs-thems/raw/master/sleak.tar.gz && tar zxvf sleak.tar.gz
-mv asset /home/panel/html
-mv view /home/panel/htm/
-echo "Enjoy using Keen Themes -jhonrex"
-
+wget https://github.com/jhonrexk/ocs-thems/raw/master/jhonrex.tar.gz && tar zxvf jhonrex.tar.gz
+mv assets /home/panel/html
+mv view /home/panel/html
+echo "FAST VPN Template Successfully Installed"
